074dbe021c69ac21e45256fe9945b4fb3d5e2f27
Arthur de Fluiter <arghhhhhthur@gmail.com>
2020-04-21 11:45:08 +0200

    added implementations for the INVLPG and reading/writing CR4

--- x86.h
+++ x86.h
@@ -18,6 +18,45 @@ static __inline uintptr_t rcr3(bool use_cache)
   return val;
 }
 
+#define CR4_VME        POW2(0)
+#define CR4_PVI        POW2(1)
+#define CR4_TSD        POW2(2)
+#define CR4_DE         POW2(3)
+#define CR4_PSE        POW2(4)
+#define CR4_PAE        POW2(5)
+#define CR4_MCE        POW2(6)
+#define CR4_PGE        POW2(7)
+#define CR4_PCE        POW2(8)
+#define CR4_OSFXSR     POW2(9)
+#define CR4_OSXMMEXCPT POW2(10)
+#define CR4_UMIP       POW2(11)
+#define CR4_VMXE       POW2(13)
+#define CR4_SMXE       POW2(14)
+#define CR4_FSGBASE    POW2(16)
+#define CR4_PCIDE      POW2(17)
+#define CR4_OSXSAVE    POW2(18)
+#define CR4_SMEP       POW2(20)
+#define CR4_SMAP       POW2(21)
+#define CR4_PKE        POW2(22)
+
+static __inline void wcr4(uintptr_t val)
+{
+  __asm __volatile("movq %0,%%cr4" : : "r"(val));
+}
+
+static __inline uintptr_t rcr4(void)
+{
+  uintptr_t val;
+  __asm __volatile("movq %%cr4,%0" : "=r"(val));
+  return val;
+}
+
+/* flushes tbl only for a specific physical page */
+static __inline void tlbflush_phys(uintptr_t addr)
+{
+  __asm __volatile("invlpg (%0)" ::"r" (addr) : "memory");
+}
+
 static __inline void tlbflush(void)
 {
   uintptr_t cr3;
